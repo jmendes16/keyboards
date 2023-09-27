c0ea954971840a85bdfa779cc64e4c60e6d126af
Joel Braganza-Mendes <113289625+jmendes16@users.noreply.github.com>
2023-09-27 11:58:49 +0100

    Added back tick on qwerty layer
    Replaced RGB toggle with back tick on qwerty layer to access menus in gaming

--- rgbkb/sol3/keymaps/sol3-codemak/keymap.c
+++ rgbkb/sol3/keymaps/sol3-codemak/keymap.c
@@ -116,7 +116,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                  KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         KC_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LPRN,                  KC_RPRN, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         XL_NUBS,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    TO(_XLnMS),               KC_RPRN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LBRC,
-        KC_LCTL,  KC_LGUI, KC_LALT, KC_PGUP, KC_PGDN, KC_SPC,  RGB_TOG, KC_LSFT, KC_ENT, KC_DEL, KC_BSPC,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_RBRC,
+        KC_LCTL,  KC_LGUI, KC_LALT, KC_PGUP, KC_PGDN, KC_SPC,  KC_GRV, KC_LSFT, KC_ENT, KC_DEL, KC_BSPC,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_RBRC,
 
         KC_VOLD, KC_VOLU, KC_VOLD, KC_VOLU, KC_VOLD, KC_VOLU,                                     KC_VOLD, KC_VOLU, KC_VOLD, KC_VOLU, KC_VOLD, KC_VOLU,
         KC_WH_D, KC_WH_U, KC_RPRN, KC_RCBR, KC_MINS,                                                       KC_WH_D, KC_WH_U, KC_RPRN, KC_LPRN, KC_MINS
